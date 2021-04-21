13afac355950cdf2fbf84312c6878c50304923bf
Федотов Валерий Валерьевич <vfedotov@msc.sigma-it.local>
2021-04-21 13:36:04 +0300

    add scripts

--- scripts/bash+sql/check_info/check_CLIENT_INFO.sh
+++ scripts/bash+sql/check_info/check_CLIENT_INFO.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+# cronrab -e
+# ### gather info regarding count of session from CLIENT_INFO
+# 30 */1 * * * /tmp/check_CLIENT_INFO.sh &>/tmp/check_CLIENT_INFO.log
+
 DB_ENV=DB.env
 if [ -d /tmp/check_CLIENT_INFO ]
   then
@@ -6,5 +11,5 @@ if [ -d /tmp/check_CLIENT_INFO ]
   else
     mkdir /tmp/check_CLIENT_INFO
     fi
-    cd ~ && . ./$DB_ENV && sqlplus / as sysdba @/tmp/check_CLIENT_INFO.sql
+    cd ~ && . ./${DB_ENV} && sqlplus / as sysdba @/tmp/check_CLIENT_INFO.sql
     cp /tmp/check_CLIENT_INFO/check_CLIENT_INFO.csv /tmp/check_CLIENT_INFO/check_CLIENT_INFO_$(date +%d_%m_%Y_%H:%M:%S).csv
---
+++ scripts/bash+sql/check_info/readme.md
@@ -0,0 +1,9 @@
+Установка:
+1) git clone в /tmp/
+2) меняем переменные в sh и sql скриптах
+
+Установка графика выполнения:
+
+# cronrab -e
+# ### gather info regarding count of session from CLIENT_INFO
+# 30 */1 * * * /tmp/check_CLIENT_INFO.sh &>/tmp/check_CLIENT_INFO.log
